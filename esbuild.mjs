df2e6d79e3b3346d6c2a48244dc3234612801ce3
sliterok <12751644+sliterok@users.noreply.github.com>
2025-12-17 21:11:17 +0100

    fix build

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,7 +1,7 @@
 import esbuild from 'esbuild';
 
 esbuild.build({
-	entryPoints: ["src/index.ts", 'src/adapter.js'],
+	entryPoints: ["src/index.ts"],
 	outdir: "dist",
 	bundle: true,
 	platform: "node",
